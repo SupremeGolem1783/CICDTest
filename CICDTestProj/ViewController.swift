f52d3f4e821c0f98b2d3f90ddb8a60a9f1a54be6
Teddy <teddy@swit.io>
2022-02-05 19:44:35 +0900

    Initail init

---
+++ CICDTestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTestProj
+//
+//  Created by 이숭인 on 2022/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
